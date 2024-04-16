08583d7397e4e137ac865b371e100840f183717c
debg48 <debgandhar4000@gmail.com>
2024-04-16 19:27:25 +0530

    get post feature added to api

--- blogs-app/app/main.py
+++ blogs-app/app/main.py
@@ -8,5 +8,13 @@ try:
     conn = psycopg2.connect(host='localhost',database='fastapi',user='postgres',password='',cursor_factory=RealDictCursor)
     cursor = conn.cursor()
     print("Database Connection Successful!")
+    
 except Exception as e :
     print(str(e))
+    
+@app.get("/posts")
+def get_posts():
+    cursor.execute("""SELECT * FROM posts""")
+    posts = cursor.fetchall()
+    return {"data":posts}
+
