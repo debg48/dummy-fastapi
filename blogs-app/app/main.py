42971751f3705d5d89dd10aad8c6b7deb2682001
debg48 <debgandhar4000@gmail.com>
2024-04-16 22:10:10 +0530

    create post feature added to api

--- blogs-app/app/main.py
+++ blogs-app/app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title : str 
+    content : str 
+    published : bool = True
 
 try:
     conn = psycopg2.connect(host='localhost',database='fastapi',user='postgres',password='',cursor_factory=RealDictCursor)
@@ -17,4 +23,11 @@ def get_posts():
     cursor.execute("""SELECT * FROM posts""")
     posts = cursor.fetchall()
     return {"data":posts}
+
+@app.post("/posts")
+def create_post(post : Post):
+    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",(post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"data": new_post}
 
