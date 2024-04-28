fde6c27317cb56a2e752b80111091d26fb8e1583
debg48 <debgandhar4000@gmail.com>
2024-04-28 10:22:04 +0530

    added created at column

--- blogs-app/app/models.py
+++ blogs-app/app/models.py
@@ -1,4 +1,6 @@
 from sqlalchemy import Column,Integer,String,Boolean
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
 from .database import Base
 
 class Post(Base):
@@ -7,4 +9,5 @@ class Post(Base):
     title = Column(String, nullable = False)
     content = Column(String, nullable = False)
     published = Column(Boolean, server_default="TRUE",nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),nullable=False,server_default=text('now()'))
 
